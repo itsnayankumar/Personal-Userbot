f8568b1d24ef8ecefb7aa80f6570372abdf11dcb
itsnayankumar <itsnayankumar10@gmail.com>
2026-02-14 12:55:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,11 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     SESSION_STRING = os.environ.get("SESSION_STRING", "")
     PORT = int(os.environ.get("PORT", 10000))
+    
+    # Telegram Channels
     DUMP_CHANNEL_ID = int(os.environ.get("DUMP_CHANNEL_ID", 0))
+    LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", 0))
+    
+    # Web Security
+    DASH_PASSWORD = os.environ.get("DASH_PASSWORD", "admin")
+    FLASK_SECRET = os.environ.get("FLASK_SECRET", "super_secret_key")
